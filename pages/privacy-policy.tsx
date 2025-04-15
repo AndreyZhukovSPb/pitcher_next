c0a3e4ce7aaa25deae620dc4e0a0cdeb1fd9649a
Andrey Zhukov <settler59@yandex.ru>
2025-04-15 15:07:28 +0200

    add peivacy policy

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,5 @@
+export default function PrivacyPopicy () {
+  return (
+    <h1>We do not collect, store, or share any personal information through this application. All data is used only within the Facebook/Instagram platform for integration purposes.</h1>
+  )
+}
